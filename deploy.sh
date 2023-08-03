d74514573a231509cba76a755d8bac85d51dad4e
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-08-03 19:35:20 +0200

    Zabbix 6.0

--- deploy.sh
+++ deploy.sh
@@ -2,35 +2,30 @@
 
 source "$(dirname "$0")/ft-util/ft_util_inc_var"
 
-$S_LOG -d $S_NAME "Start $S_NAME $*"
-
-SUDOERS_ETC="/etc/sudoers.d/ft-git"
+sudoers_etc="/etc/sudoers.d/ft-git"
 
 echo "
   SETUP SUDOER FILES
 ------------------------------------------"
 
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
+$S_LOG -d $S_NAME -d "$sudoers_etc" "==============================="
 
-if [ -f /etc/debian_version ] # Debian
-then
-    echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$SUDOERS_ETC &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:/usr/bin/git" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
+if [ -f /etc/debian_version ]; then # Debian
+    echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$sudoers_etc &>/dev/null
+    echo "zabbix ALL=(ALL) NOPASSWD:/usr/bin/git" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
+    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
+    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
 
-elif uname --all | grep synology >/dev/null # Synology DSM
-then
-    echo "Defaults:zabbix !requiretty" > "${SUDOERS_ETC}"
-    echo "zabbix ALL=(ALL) NOPASSWD:/bin/git" >> "${SUDOERS_ETC}"
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" >> "${SUDOERS_ETC}"
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" >> "${SUDOERS_ETC}"
-    chmod 0440 "$SUDOERS_ETC"
+elif uname --all | grep synology >/dev/null; then # Synology DSM
+    echo "Defaults:zabbix !requiretty" >"${sudoers_etc}"
+    echo "zabbix ALL=(ALL) NOPASSWD:/bin/git" >>"${sudoers_etc}"
+    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" >>"${sudoers_etc}"
+    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" >>"${sudoers_etc}"
+    chmod 0440 "$sudoers_etc"
 fi
 
-cat $SUDOERS_ETC | $S_LOG -d "$S_NAME" -d "$SUDOERS_ETC" -i 
-
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
+cat $sudoers_etc | $S_LOG -d "$S_NAME" -d "$sudoers_etc" -i
+$S_LOG -d $S_NAME -d "$sudoers_etc" "==============================="
 
 $S_LOG -d "$S_NAME" "End $S_NAME"
 
