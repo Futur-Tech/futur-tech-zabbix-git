8a49400873c3269190cd0e616ed8d6374e2b88c7
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-08-14 12:59:24 +0200

    ft_util_sudoersd

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 
 source "$(dirname "$0")/ft-util/ft_util_inc_var"
+source "$(dirname "$0")/ft-util/ft_util_inc_func"
+source "$(dirname "$0")/ft-util/ft_util_sudoersd"
 
-sudoers_etc="/etc/sudoers.d/ft-git"
-
-echo "
-  SETUP SUDOER FILES
-------------------------------------------"
-
-$S_LOG -d $S_NAME -d "$sudoers_etc" "==============================="
+app_name="ft-git"
 
 if [ -f /etc/debian_version ]; then # Debian
-    echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$sudoers_etc &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:/usr/bin/git" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" | sudo EDITOR='tee -a' visudo --file=$sudoers_etc &>/dev/null
-
-elif uname --all | grep synology >/dev/null; then # Synology DSM
-    echo "Defaults:zabbix !requiretty" >"${sudoers_etc}"
-    echo "zabbix ALL=(ALL) NOPASSWD:/bin/git" >>"${sudoers_etc}"
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/deploy-update.sh" >>"${sudoers_etc}"
-    echo "zabbix ALL=(ALL) NOPASSWD:${S_DIR_PATH}/git-all *" >>"${sudoers_etc}"
-    chmod 0440 "$sudoers_etc"
+    echo "
+  SETUP SUDOER FILES
+------------------------------------------"
+    bak_if_exist "/etc/sudoers.d/${app_name}"
+    sudoersd_reset_file "$app_name" "zabbix"
+    sudoersd_addto_file "$app_name" "zabbix" "${S_DIR_PATH}/deploy-update.sh"
+    sudoersd_addto_file "$app_name" "zabbix" "${S_DIR_PATH}/git-all fetch"
+    sudoersd_addto_file "$app_name" "zabbix" "${S_DIR_PATH}/git-all qstatus"
+    show_bak_diff_rm "/etc/sudoers.d/${app_name}"
 fi
-
-cat $sudoers_etc | $S_LOG -d "$S_NAME" -d "$sudoers_etc" -i
-$S_LOG -d $S_NAME -d "$sudoers_etc" "==============================="
-
-$S_LOG -d "$S_NAME" "End $S_NAME"
 
 exit
