6fc051a4053c8e81146f9f5395be277358e09873
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-08-14 15:34:28 +0200

    gitall userparameter

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,29 @@ source "$(dirname "$0")/ft-util/ft_util_sudoersd"
 
 app_name="ft-git"
 
-if [ -f /etc/debian_version ]; then # Debian
-    echo "
+# Checking which Zabbix Agent is detected and adjust include directory
+$(which zabbix_agent2 >/dev/null) && zbx_conf_agent_d="/etc/zabbix/zabbix_agent2.d"
+$(which zabbix_agentd >/dev/null) && zbx_conf_agent_d="/etc/zabbix/zabbix_agentd.conf.d"
+if [ ! -d "${zbx_conf_agent_d}" ]; then
+    $S_LOG -s warn -d $S_NAME "${zbx_conf_agent_d} Zabbix Include directory not found"
+    exit 1
+fi
+
+echo "
+  INSTALL ZABBIX CONF
+------------------------------------------"
+
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/etc.zabbix/${app_name}.conf" "${zbx_conf_agent_d}/${app_name}.conf"
+
+echo "
   SETUP SUDOER FILES
 ------------------------------------------"
-    bak_if_exist "/etc/sudoers.d/${app_name}"
-    sudoersd_reset_file $app_name zabbix
-    sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/deploy-update.sh"
-    sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/git-all fetch"
-    sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/git-all qstatus"
-    show_bak_diff_rm "/etc/sudoers.d/${app_name}"
-fi
+
+bak_if_exist "/etc/sudoers.d/${app_name}"
+sudoersd_reset_file $app_name zabbix
+sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/deploy-update.sh"
+sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/git-all fetch"
+sudoersd_addto_file $app_name zabbix "${S_DIR_PATH}/git-all qstatus"
+show_bak_diff_rm "/etc/sudoers.d/${app_name}"
 
 exit
